9e575395f174e455ac40f55ad1b1d37b01982348
Christopher Motl <cmotl@cmotl.com>
2011-01-18 19:51:31 -0600

    inital commit

---
+++ Classes/ScrollViewSubClassWithDelegateViewController.h
@@ -0,0 +1,16 @@
+//
+//  ScrollViewSubClassWithDelegateViewController.h
+//  ScrollViewSubClassWithDelegate
+//
+//  Created by Christopher Motl on 1/18/11.
+//  Copyright 2011 cmotl.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollViewSubClassWithDelegateViewController : UIViewController {
+
+}
+
+@end
+
